57e87c447fa5ec87c01315de6f5074bbc1739e78
nabaraj malla <nabarajmalla4000@gmail.com>
2024-07-06 11:23:46 +0545

    mongoURL corrected

--- config/db.js
+++ config/db.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
+const dotenv = require("dotenv");
+dotenv.config();
+const url = process.env.MONGO_URL;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/bloodBank");
+    // await mongoose.connect("mongodb://localhost:27017/bloodBank");
+    console.log(url);
+    await mongoose.connect(url);
     // console.log(`connected to Mongodb database ${mongoose.connection.host}`);
     console.log("databse connected successfully");
   } catch (error) {
