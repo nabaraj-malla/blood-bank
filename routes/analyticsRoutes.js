3396a7385bf1b170a79f9d72a15d6712122d07f7
nabaraj malla <nabarajmalla4000@gmail.com>
2024-07-06 07:11:09 +0545

    corrected in authMiddleware

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,29 +1,10 @@
 const express = require("express");
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware.js");
 const {
   bloodGroupDetailsController,
-} = require("../controllers/analyticsController");
-<<<<<<< HEAD
-// const {
-//   bloodGroupDetailsControllerDemo,
-// } = require("../controllers/consumerAnalyticsController");
-=======
-const {
-  bloodGroupDetailsControllerDemo,
-} = require("../controllers/consumerAnalyticsController");
->>>>>>> 37f1810a8e3a7fb4189847d110ad9c5157d16f1b
+} = require("../controllers/analyticsController.js");
 
 const router = express.Router();
-
-// routes
-
 // GET BLOOD DATA
-// /bloodGroupData(we can write here anaything)
 router.get("/bloodGroups-data", authMiddleware, bloodGroupDetailsController);
-<<<<<<< HEAD
-// router.get("/bloodGroupsDemo", authMiddleware, bloodGroupDetailsControllerDemo);
-=======
-router.get("/bloodGroupsDemo", authMiddleware, bloodGroupDetailsControllerDemo);
->>>>>>> 37f1810a8e3a7fb4189847d110ad9c5157d16f1b
-
 module.exports = router;
