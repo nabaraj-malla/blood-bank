d256d08d01c95ac29513649521960c40de2db34f
nabaraj malla <nabarajmalla4000@gmail.com>
2024-07-08 12:22:59 +0545

    connected to mongodb cloud

--- config/db.js
+++ config/db.js
@@ -3,11 +3,20 @@ const colors = require("colors");
 const dotenv = require("dotenv");
 dotenv.config();
 const url = process.env.MONGO_URL;
+console.log(url);
 
 const connectDB = async () => {
   try {
     // await mongoose.connect("mongodb://localhost:27017/bloodBank");
-    console.log(url);
+    // await mongoose
+    //   .connect
+    // "mongodb+srv://newUser:QUmQq4fKeKVnpvTT@cluster0.ly5tjke.mongodb.net/college?retryWrites=true&w=majority&appName=Cluster0"
+    // "mongodb+srv://nabarajmalla4000:Atl@s2057@cluster0.ly5tjke.mongodb.net/college?retryWrites=true&w=majority&appName=Cluster0"
+
+    // "mongodb+srv://nabarajmalla4000:thakuri12000@cluster0.ly5tjke.mongodb.net/demobloodbank?retryWrites=true&w=majority&appName=Cluster0"
+    // ();
+
+    // console.log(url);
     await mongoose.connect(url);
     // console.log(`connected to Mongodb database ${mongoose.connection.host}`);
     console.log("databse connected successfully");
